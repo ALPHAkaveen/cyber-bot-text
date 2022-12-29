b201c28e11cd589974694a72ff77ae03b331df52
ALPHAkaveen <103016362+ALPHAkaveen@users.noreply.github.com>
2022-12-29 14:22:25 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,23 +15,24 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['94774579505'] //ur owner number
-global.ownername = "Nimesh" //ur owner name
-global.ytname = "YT: nima" //ur yt chanel name
-global.socialm = "GitHub: kavee" //ur github or insta name
+global.alivepic = `https://i.ibb.co/K0RtK0Z/IMG-20221021-WA0089.jpg `
+global.owner = ['94774071805'] //ur owner number
+global.ownername = "DarkTem" //ur owner name
+global.ytname = "YT: kavee" //ur yt chanel name
+global.socialm = "Dark nero" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
-global.ownernomer = "94774579505" //ur number
-global.premium = ['94775679505'] //ur premium number
-global.botname = 'DarkNero Bot MD V8' //ur bot name
+global.ownernomer = "94774071805" //ur number
+global.premium = ['94774071805'] //ur premium number
+global.botname = 'DarkNero Bot MD V7' //ur bot name
 global.linkz = "https://chat.whatsapp.com/HYj9wu5Jrv6CROxyeQbHoS" //your theme url which will be displayed on whatsapp
 global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD6' //script link
 global.themeemoji = "🦄" //ur theme emoji
 global.packname = "Sticker By" //ur sticker watermark packname
 global.author = "Cheems Bot\n\n\nA whatsapp bot developed by\n🦄Dream Guy Xeon\n\n\n\n\n\n\n\n       --------______-------\n\n\n\n\n\n\n\nContact No. : +916909137213" //ur sticker watermark author
-global.wm = "Xeon Bot Inc." //ur watermark
+global.wm = "Dark Bot Inc." //ur watermark
 
 // Other
 global.sessionName = 'session'
