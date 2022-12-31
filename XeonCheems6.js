58fe41ee785a9f6c3bec2bc5c9372e2a8d27d6c3
ALPHAkaveen <103016362+ALPHAkaveen@users.noreply.github.com>
2022-12-31 09:42:08 +0530

    Update XeonCheems6.js

--- XeonCheems6.js
+++ XeonCheems6.js
@@ -3909,7 +3909,8 @@ case 'setthumb':{
 
 case'ai':
 case'Hi':
-case '.alive':
+case 'alive':
+case'Gn':    
 XeonBotInc_dev = await getBuffer(`https://github.com/nimaofficial/Server/raw/main/Audio/${command}.mp3`)
 await XeonBotInc.sendMessage(m.chat, { audio: XeonBotInc_dev, mimetype: 'audio/mp4', ptt: true }, { quoted: m })     
 break
