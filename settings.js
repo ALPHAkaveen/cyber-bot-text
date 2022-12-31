14d69042a5edc230d52b237d173555082721d54d
NIMA OFFICIAL <110774656+nimaofficial@users.noreply.github.com>
2022-12-31 08:38:11 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -17,9 +17,9 @@ global.doc6 = 'application/vnd.android.package-archive'
 //owmner v card
 global.alivepic = `https://i.ibb.co/K0RtK0Z/IMG-20221021-WA0089.jpg `
 global.owner = ['94774071805'] //ur owner number
-global.ownername = "DarkTem" //ur owner name
+global.ownername = "Kaveesha Sithum" //ur owner name
 global.ytname = "YT: kavee" //ur yt chanel name
-global.socialm = "Dark nero" //ur github or insta name
+global.socialm = "Dark Nero" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
@@ -29,7 +29,7 @@ global.botname = 'DarkNero Bot MD V7' //ur bot name
 global.linkz = "https://chat.whatsapp.com/HYj9wu5Jrv6CROxyeQbHoS" //your theme url which will be displayed on whatsapp
 global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD6' //script link
-global.themeemoji = "🦄" //ur theme emoji
+global.themeemoji = "🐶" //ur theme emoji
 global.packname = "Sticker By" //ur sticker watermark packname
 global.author = "Dark Nero\n\n\nA whatsapp bot developed by\n🐶Cyber X Team" //ur sticker watermark author
 global.wm = "ᴅᴀʀᴋ ɴʀᴏ ɪɴᴄ." //ur watermark
